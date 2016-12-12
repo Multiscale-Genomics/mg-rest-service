bdd00501905884575ff045b4ca16df8b2ad1a395
Mark McDowall <mark.mcdowall@gmail.com>
2016-12-12 15:19:33 +0000

    Initial commit of service scripts

---
+++ rest/app.py
@@ -0,0 +1,105 @@
+"""
+.. Copyright 2016 EMBL-European Bioinformatics Institute
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+
+from flask import Flask, make_response, request
+from flask_restful import Api, Resource
+from flask_apscheduler import APScheduler
+
+from dmp import dmp
+from dmp import rest
+
+import logging
+logging.basicConfig()
+
+class Config(object):
+    import json
+
+    with open('./registry.json') as data_file:    
+        data = json.load(data_file)
+    
+    JOBS = [
+        {
+            'id': 'ping',
+            'func': 'rest.jobs:ping',
+            'args' : ("registry", data),
+            'trigger': 'interval',
+            'seconds': 60
+        }
+    ]
+
+    SCHEDULER_VIEWS_ENABLED = True
+
+
+app = Flask(__name__)
+app.config.from_object(Config())
+
+scheduler = APScheduler()
+scheduler.init_app(app)
+scheduler.start()
+
+
+class GetEndPoints(Resource):
+    """
+    Class to handle the http requests for returning information about the end
+    points
+    """
+    
+    def get(self):
+        r = rest.rest()
+        services = r.get_available_services()
+        
+        links = {'_self' : request.url_root + 'api'}
+        for service in services:
+            links['_' + service] = request.url_root + 'api/' + service
+        return {
+            '_links': links
+        }
+
+
+class ping(Resource):
+    """
+    Class to handle the http requests to ping a service
+    """
+    
+    def get(self):
+        import release
+        res = {
+            "status":  "ready",
+            "version": release.__version__,
+            "author":  release.__author__,
+            "license": release.__license__,
+            "name":    release.__rest_name__,
+            "description": release.__description__
+        }
+        return res
+
+"""
+Define the URIs and their matching methods
+"""
+api = Api(app)
+
+#   List the available end points for this service
+api.add_resource(GetEndPoints, "/api", endpoint='service-root')
+
+#   Service ping
+api.add_resource(ping, "/api/ping", endpoint='service-ping')
+
+
+"""
+Initialise the server
+"""
+if __name__ == "__main__":
+    app.run(port=5002, debug=True, use_reloader=False)
---
+++ rest/jobs.py
@@ -0,0 +1,60 @@
+"""
+.. Copyright 2016 EMBL-European Bioinformatics Institute
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+
+from urllib2 import Request, urlopen, URLError
+from dmp import rest
+import json
+
+def ping(a, b):
+    """
+    Ping the defined end points to determine if they are valid services. The
+    results are then logged and used by the DMP and other service when listing
+    what end points are available. This allows for services to drop out and not
+    have nodes pointing to those services until they come back.
+    
+    Parameters
+    ----------
+    a : str
+        key for the registry dict
+    b : dict
+        'a' is the registry key
+    """
+    r = rest.rest()
+    
+    for service in b[str(a)]:
+        req = Request(service["url"])
+        status = "down"
+        log = ""
+        data = {}
+        try:
+            response = urlopen(req)
+            log = response.code
+            data = json.loads(response.read())
+            response.close()
+        except URLError, e:
+            valid = False
+            log = e.code
+        
+        if log == 200:
+            status = "up"
+        
+        if r.is_service(service["name"]) == True:
+            r.set_service_status(service["name"], status)
+        else:
+            description = data["description"] if data.has_key("description") else ""
+            r.add_service(service["name"], service["url"], description, status)
+        
+        #print service["name"] + " - " + str(log)
