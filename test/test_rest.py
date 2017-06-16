b84a1e0e6a52b11c69943121a7dbbcb6f234b88a
Mark McDowall <mark.mcdowall@gmail.com>
2017-06-16 11:35:38 +0100

    Run pylint to help tidy up the code

--- test/test_rest.py
+++ test/test_rest.py
@@ -1,12 +1,12 @@
 """
 .. Copyright 2017 EMBL-European Bioinformatics Institute
-   
+
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
-   
+
        http://www.apache.org/licenses/LICENSE-2.0
-   
+
    Unless required by applicable law or agreed to in writing, software
    distributed under the License is distributed on an "AS IS" BASIS,
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,19 +15,25 @@
 """
 
 import os
-import pytest
 import tempfile
 import json
+import pytest
 
 from context import app
 
 @pytest.fixture
 def client(request):
+    """
+    Definges the client object to make requests against
+    """
     db_fd, app.APP.config['DATABASE'] = tempfile.mkstemp()
     app.APP.config['TESTING'] = True
     client = app.APP.test_client()
-    
+
     def teardown():
+        """
+        Close the client once testing has completed
+        """
         os.close(db_fd)
         os.unlink(app.APP.config['DATABASE'])
     request.addfinalizer(teardown)
@@ -35,13 +41,19 @@ def client(request):
     return client
 
 def test_endpoints(client):
-    rv = client.get('/mug/api')
-    details = json.loads(rv.data)
+    """
+    Test that the root endpoint is returning the expected keys
+    """
+    rest_value = client.get('/mug/api')
+    details = json.loads(rest_value.data)
     #print(details)
     assert '_links' in details
 
 def test_ping(client):
-    rv = client.get('/mug/api/ping')
-    details = json.loads(rv.data)
+    """
+    Test that the ping function is returning the status key
+    """
+    rest_value = client.get('/mug/api/ping')
+    details = json.loads(rest_value.data)
     #print(details)
     assert 'status' in details
