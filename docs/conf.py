868d2fc6f6198c956f26a2dcc4202b2ef81fc29c
Mark McDowall <mark.mcdowall@gmail.com>
2017-06-30 13:41:47 +0100

    Spoofed the DM API for the RTD docs build process

--- docs/conf.py
+++ docs/conf.py
@@ -19,6 +19,16 @@
 import os, sys
 sys.path.insert(0, os.path.abspath('..'))
 
+from mock import Mock as MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return MagicMock()
+
+MOCK_MODULES = ['pyBigWig', 'dmp']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 
 
 # -- General configuration ------------------------------------------------
